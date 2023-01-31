dea163c179d3ab3daa7ae08dad86be2fd35a0aa7
mahdi-21 <83784541+mahdi-21@users.noreply.github.com>
2023-01-31 18:48:12 +0300

    Add files via upload

---
+++ first_ch.js
@@ -0,0 +1,31 @@
+var titleOne = "First Challenge";
+var descriptionOne = "ُWeb developement course";
+var dateOne = "25/5";
+
+document.write(`<div id="first">
+                <h3 id="first_h3">${titleOne}</h3>
+                <p>${descriptionOne}</p>
+                <span>${dateOne}</span>
+                </div>`);
+
+document.write(`<div id="second">
+                <h3 id="second_h3">${titleOne}</h3>
+                <p>${descriptionOne}</p>
+                <span>${dateOne}</span>
+                </div>`);
+
+document.write(`<div id="third">
+                <h3 id="third_h3">${titleOne}</h3>
+                <p>${descriptionOne}</p>
+                <span>${dateOne}</span>
+                </div>`);
+
+document.write(`<div id= "fourth">
+                <h3 id="fourth_h3">${titleOne}</h3>
+                <p>${descriptionOne}</p>
+                <span>${dateOne}</span>
+                </div>`);
+document.querySelector("h3").style.color = "blue";
+document.querySelector("h3").style.color = "blue";
+document.querySelector("div").style.color = "red";
+document.querySelector("p").style.color = "purple";
